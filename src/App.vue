4aa319ecd0afd4bd068ac9918bf34d33a4e6a60c
tianaFomicheva <tianirika@gmail.com>
2021-08-12 12:46:39 +0500

    структура, апи, начало приведения данных к удобным форматам

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import { getData } from './api/api.js'
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+    return{
+      apiData:[],
+      typeGroups:{}
+    }
+  },
+
+  
+  created(){
+    getData(data=>{this.apiData.push(data)})
+     const types = Object.keys(this.$options.OPTIONS_CODES)
+  const emptyArr = []
+  for(var i = 0; i<types.length; i++){
+emptyArr.push([])
+  }
+ for(var j = 0; j<types.length; j++){
+   this.typeGroups[types[j]] = emptyArr[j]
+  
+ }
+    Object.entries(this.typeGroups).map(([key,val])=>{val.push("1"); console.log(key, val)})
+    
+  },
+  OPTIONS_CODES:{
+    "0": "подразделений ФМС",
+    "1": "ГУВД или МВД региона",
+    "2":"УВД или ОВД района или города",
+    "4": "отделений полиции"
+  },
+  computed:{
+    itemsGroups(){
+
+      console.log(this.apiData.filter(item => Object.keys(this.$options.OPTIONS_CODES).includes(item.data.type)))
+      return this.apiData.filter(item => Object.keys(this.$options.OPTIONS_CODES).includes(item.data.type))
+
+    }
+  }
+}
+</script>
+
---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fms_unit";
+var token = "2aeccb75933c65d7264bca4a522c2d6ba91e8970";
+var query = "772 053";
+
+var options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Token " + token
+    },
+    body: JSON.stringify({query: query})
+}
+
+export const getData = (cb)=> {
+    
+  fetch(url, options)
+.then(response => response.json())
+.then(result => {cb(result.suggestions)})
+.catch(error => console.log("error", error))
+}
