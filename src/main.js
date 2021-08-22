23a2a2f6a1c7abcb7ef5e8cb08eb4307c4174fd8
tianaFomicheva <tianirika@gmail.com>
2021-08-22 23:41:37 +0500

    дабаг сборки при подгрузке токена из env

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import '../.env'
 import './custom-reset.css'
 import './assets/scss/main.scss'
 import './assets/scss/reset.scss'
